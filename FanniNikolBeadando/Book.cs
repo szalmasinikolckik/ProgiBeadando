cc144d8d5790ad80c958f67fdb6c1e594815dd5c
szalmasi.nikol <szalmasi.nikol@ckik.hu>
2026-05-08 09:21:05 +0200

    elkeztem

---
+++ FanniNikolBeadando/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FanniNikolBeadando
+{
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Pages { get; set; }
+        public bool Favorite { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Author}";
+        }
+    }
+}
